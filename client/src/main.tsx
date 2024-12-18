5221d2972429ffa220088c5053c361aa6c114469
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-19 04:38:58 +0800

    feat: installed react query and axios and wrap provider the paraphrasing page component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,12 +5,31 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 
 import ParaphrasingPage from './pages/Paraphrasing'
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    
       <Routes>
-        <Route index element={<ParaphrasingPage />}/>
+          <Route 
+            index 
+            path='/paraphrasing-tool' 
+            element={
+              <QueryClientProvider client={queryClient}>
+                <ParaphrasingPage />
+              </QueryClientProvider>
+            }
+          />
+        
       </Routes>
+
     </BrowserRouter>
   </StrictMode>,
 )

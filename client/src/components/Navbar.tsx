256727c78e8ce5013055c5a981a9cb8734e92ce2
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-18 13:04:31 +0800

    feat: applied the shadcn dialog and menu component for the auth UI

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,35 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import quickquillLogo from "../assets/img/quickquill_logo.png";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { useState } from "react";
+
+import { 
+    ContextMenu, 
+    ContextMenuContent, 
+    ContextMenuItem 
+} from '@/components/ui/context-menu';
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogFooter
+} from "@/components/ui/dialog"
+
+import QuickQuillLogo from '../assets/img/quickquill_logo.png';
+import GoogleLogo from '../assets/img/google.webp';
+import FacebookLogo from '../assets/img/facebook.png';
+
+import { Button } from "./ui/button";
+
 
 export function NavBar() {
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+    const [isAuthenticated, setisAuthenticated] = useState<boolean>(false);
+
     return (
         <div className="fixed top-0 border-b-2 w-full h-[6rem] flex items-center bg-white font-semibold px-6">
             
@@ -20,11 +47,89 @@ export function NavBar() {
                 Paraphrasing Tool
             </h1>
 
-            <div className="flex items-center justify-end flex-1">
-                <div className="bg-gray-100 p-2 rounded-full hover:cursor-pointer">
-                    <FontAwesomeIcon icon={faUser} />
-                </div>
+            <div className="flex items-center justify-end flex-1 font-semibold">
+
+                {
+                    !isAuthenticated 
+                    ? ( <AuthDialog />)
+
+                    : (
+                        <div className="bg-gray-100 p-2 rounded-full hover:cursor-pointer">
+                            <FontAwesomeIcon 
+                                onClick={() => setIsMenuOpen(true)}
+                                icon={faUser} 
+                            />
+                        </div>
+                    )
+                }
+                
             </div>
+            
+            {
+                isMenuOpen && (
+                    <ContextMenu modal={true}>
+                        <ContextMenuContent onClick={() => setIsMenuOpen(false)}>
+                            <ContextMenuItem onClick={() => console.log('Item 1 clicked')}>Item 1</ContextMenuItem>
+                            <ContextMenuItem onClick={() => console.log('Item 2 clicked')}>Item 2</ContextMenuItem>
+                        </ContextMenuContent>
+                    </ContextMenu>)
+            }
+
         </div>
+    );
+}
+
+
+function AuthDialog(){
+    return(
+        <Dialog>
+            <DialogTrigger asChild>
+                <h1 className="text-lg text-slate-800 hover:cursor-pointer hover:opacity-75">
+                    Login/Signup
+                </h1>
+            </DialogTrigger>
+            
+            <DialogContent className="w-full p-16 bg-white ">
+                <DialogHeader>
+
+                    <DialogTitle className="flex items-center justify-center mb-5">
+                        <img src={QuickQuillLogo} alt="QuickQuill Logo" width={70} height={70} />
+                        <h1 className="text-3xl text-blue-800">QuickQuill</h1>
+                    </DialogTitle>
+
+                    <DialogDescription className="text-center text-slate-800 text-3xl ">
+                        Write Without Limits
+                    </DialogDescription>
+
+                </DialogHeader>
+
+                <div className="w-full flex flex-col justify-center items-center gap-5 mt-8">
+                    <Button 
+                        className="w-full rounded-full p-5 border-2 border-gray-400"
+                    >
+                        <img src={GoogleLogo} alt="Google Logo" width={30}/>
+                        <h1>Continue with Google</h1>
+                    </Button>
+
+                    <Button 
+                        className="w-full rounded-full p-5 border-2 border-gray-400"
+                    >
+                        <img src={FacebookLogo} alt="Google Logo" width={30}/>
+                        <h1>Continue with Facebook</h1>
+
+                    </Button>
+                </div>
+
+                <DialogFooter className="font-semibold mt-8 text-sm ">
+                    <h1>
+                        By continuing, you agree to  
+                        <span className="mx-1 text-blue-800 hover:cursor-pointer hover:opacity-75 ">Terms of Service</span> 
+                        and have read our 
+                        <span className="mx-1 text-blue-800 hover:cursor-pointer hover:opacity-75">Privacy Policy.</span>
+                    </h1>
+                </DialogFooter>
+                
+            </DialogContent>
+        </Dialog>
     );
 }

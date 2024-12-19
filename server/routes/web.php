c8773a2fc6de953cc9636449f6354a5d144d0cca
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-19 16:46:49 +0800

    feat: setup protected routes for login and paraphrase

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::middleware('guest')->group(function (){
+    Route::get('auth/google', [AuthController::class, 'redirectToGoogle']);
+    Route::get('auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
 });
+
+

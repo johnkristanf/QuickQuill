33fe46cad2fac6475fa47ace54197a93facfc4ec
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-19 16:43:49 +0800

    feat: imported the auth and paraphrase services

--- server/app/Providers/AppServiceProvider.php
+++ server/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\AuthService;
+use App\Services\ParaphraseService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(AuthService::class, function ($app) {
+            return new AuthService();
+        });
+
+        $this->app->singleton(ParaphraseService::class, function ($app) {
+            return new ParaphraseService();
+        });
     }
 
     /**

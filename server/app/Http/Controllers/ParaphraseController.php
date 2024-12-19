8a95949f5d02961efb59f4ed1108f132f64235ca
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-19 16:36:42 +0800

    feat: implemented the paraphrasing controller

---
+++ server/app/Http/Controllers/ParaphraseController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ParaphraseService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class ParaphraseController extends Controller
+{
+    protected $paraphraseService;
+
+    public function __construct(ParaphraseService $paraphraseService)
+    {
+        $this->paraphraseService = $paraphraseService;
+    }
+
+    public function paraphrase(Request $request)
+    {
+        $validated = $request->validate([
+            'original_text' => 'required|string',
+            'paraphrase_mode' => 'required|string',
+        ]);
+
+        $paraphrasedText = $this->paraphraseService->paraphraseText($validated['original_text'], $validated['paraphrase_mode']);
+        $isSavedHistory = $this->paraphraseService->saveHistory("paraphrase", $validated['original_text'], $paraphrasedText);
+
+        return response()->json([
+            'paraphrased_text' => $paraphrasedText,
+            'saved_history' => $isSavedHistory ? true : false
+        ], 200);
+    }
+}

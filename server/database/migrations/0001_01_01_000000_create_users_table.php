0a7c3261dc4ed080f1606b9e998b7fd362935f66
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-19 16:46:14 +0800

    feat: setup google creadentials and columns for db insertion

--- server/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+
+            $table->string('avatar')->nullable();
+            $table->string('google_id')->nullable();
+            $table->string('facebook_id')->nullable();
+            $table->string('provider');
+            
             $table->rememberToken();
             $table->timestamps();
         });

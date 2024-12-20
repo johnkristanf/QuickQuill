919d7c8139abc2a4a3f91c461d9b0762baa581c9
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-20 16:49:49 +0800

    feat: setup[ the lifespan function for the model warming job

--- python/main.py
+++ python/main.py
@@ -1,18 +1,41 @@
+import asyncio
+import uvicorn
+
 from fastapi import FastAPI
-from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+
 from dotenv import load_dotenv
 from middleware.api_token import JWTMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 
-# from generative.paraphrase import GenerativeParaphrase
-from generative.test_paraphrase import paraphrase_text
+from type.paraphrase import Paraphrase
+from generative.paraphrase import GenerativeParaphrase
 from generative.warming_model import keep_model_warm
 
 load_dotenv()
 
 
-app = FastAPI()
-# gen = GenerativeParaphrase()
+# LIFESPAN USAGE: LOADING A MACHINE LEARNING MODEL 
+# BEFORE THE USER CAN MAKE AN API REQUEST 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(keep_model_warm, "interval", days=1)
+    scheduler.start()
+    print("Scheduler started.")
+
+    try:
+        yield
+    finally:
+        print("Shutting down scheduler.")
+        scheduler.shutdown()
+
+
+app = FastAPI(lifespan=lifespan)
+gen = GenerativeParaphrase()    
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,14 +47,14 @@ app.add_middleware(
 
 app.add_middleware(JWTMiddleware)
 
-class Paraphrase(BaseModel):
-    original_text: str
-    paraphrase_mode: str
-
 
 @app.post("/paraphrase")
 async def paraphrase(data: Paraphrase):
-    paraphrased_text = paraphrase_text(data.original_text, data.paraphrase_mode)
+    paraphrased_text = gen.paraphrase(data.original_text, data.paraphrase_mode)
     return { "paraphrased_text": paraphrased_text }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=5000, log_level="info")
 
 # uvicorn main:app --port 5000 --reload

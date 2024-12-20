e5df0deaf505ca09772e2e1c7b1193e16786719a
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-20 13:03:59 +0800

    feat: applied tokenizer batching

--- python/generative/paraphrase.py
+++ python/generative/paraphrase.py
@@ -1,58 +1,88 @@
-from transformers import PegasusTokenizer, PegasusForConditionalGeneration
 import torch
+import nltk
 
+from transformers import PegasusTokenizer, PegasusForConditionalGeneration
+from torch.amp import autocast
+from accelerate import Accelerator
+
+accelerator = Accelerator()
 torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+torch.cuda.empty_cache()
+
+print("Loading model...")
+
+model = PegasusForConditionalGeneration.from_pretrained('tuner007/pegasus_paraphrase').to(torch_device)
+tokenizer = PegasusTokenizer.from_pretrained('tuner007/pegasus_paraphrase')
+model, tokenizer = accelerator.prepare(model, tokenizer)
+
+print("Model loaded successfully.")
+
+
+nltk.download('punkt_tab')
+
 
 class GenerativeParaphrase:
-    def __init__(self):
-        try:
-            
-            self.pre_trained_model = 'tuner007/pegasus_paraphrase'
-            print("Loading model...")
-            self.model = PegasusForConditionalGeneration.from_pretrained(self.pre_trained_model).to(torch_device)
-            self.tokenizer = PegasusTokenizer.from_pretrained(self.pre_trained_model)
-            print("Model loaded successfully.")
 
-        except Exception as e:
-            print(f"Error loading model: {e}")
-            raise e
+    def paraphrase_text(self, text: str, paraphrase_mode: str):
 
-    def paraphrase_text(self, text, paraphrase_mode):
-        token_ids = self.tokenizer.encode(text, return_tensors='pt')
+        sentences = nltk.sent_tokenize(text)
+        print(f"Sentences: {sentences}")
 
-        print('token_ids: ', token_ids)
 
-        summary_ids_standard = None
-            
-        if paraphrase_mode == 'standard':
-            summary_ids_standard = self.model.generate(
-                token_ids, 
-                max_length=256, 
-                num_beams=3, 
-                no_repeat_ngram_size=2 
-            )
-            
-        elif paraphrase_mode == 'creative':
-            summary_ids_standard = self.model.generate(
-                token_ids, 
-                max_length=256, 
-                temperature=1.0, 
-                do_sample=True,
-                top_k=50, 
-                num_beams=3, 
-                no_repeat_ngram_size=2
-            )
+        batch_size = 8
+        paraphrased_text = []
 
-        else:
-            summary_ids_standard = self.model.generate(
-                token_ids, 
-                max_length=256, 
-                do_sample=True,
-                top_p=0.9, 
-                num_beams=3, 
-                no_repeat_ngram_size=2
-            )
 
-        paraphrased_text = self.tokenizer.decode(summary_ids_standard[0], skip_special_tokens=True)
+        for i in range(0, len(sentences), batch_size):
+            sentences_batch = sentences[i:i + batch_size]
 
-        return paraphrased_text
+            result = tokenizer.batch_encode_plus(sentences_batch, return_tensors='pt', padding=True, truncation=True)
+            print('result: ', result)
+
+            summary_ids_standard = None
+                
+            with autocast(torch_device):
+                if paraphrase_mode.lower() == 'standard':
+
+                    initial_memory = torch.cuda.memory_allocated()
+                    print(f"Initial memory usage: {initial_memory / (1024 ** 3)} GB")
+
+                    summary_ids_standard = model.generate(
+                        input_ids=result['input_ids'], 
+                        max_length=256, 
+                        num_beams=2, 
+                        no_repeat_ngram_size=2 
+                    )
+
+                    final_memory = torch.cuda.memory_allocated()
+                    print(f"Final memory usage: {final_memory / (1024 ** 3)} GB")
+                    print(f"Memory usage for this batch: {(final_memory - initial_memory) / (1024 ** 3)} GB")
+                                        
+                elif paraphrase_mode.lower() == 'creative':
+                    summary_ids_standard = model.generate(
+                        input_ids=result['input_ids'], 
+                        max_length=256, 
+                        temperature=1.0, 
+                        do_sample=True,
+                        top_k=50, 
+                        num_beams=3, 
+                        no_repeat_ngram_size=2
+                    )
+
+                else:
+                    summary_ids_standard = model.generate(
+                        input_ids=result['input_ids'], 
+                        max_length=256, 
+                        do_sample=True,
+                        top_p=0.9, 
+                        num_beams=3, 
+                        no_repeat_ngram_size=2
+                    )
+
+            paraphrased_batch  = [tokenizer.decode(id, skip_special_tokens=True) for id in summary_ids_standard]
+            paraphrased_text.extend(paraphrased_batch)
+
+            concatenated_paraphrased_text = ' '.join(paraphrased_text)
+            print(f'paraphrased_text: {concatenated_paraphrased_text}')
+
+        return concatenated_paraphrased_text

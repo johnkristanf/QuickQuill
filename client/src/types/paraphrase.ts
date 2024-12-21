7ad5476d92b24b0a5143355f9ca6fbe98e6dda90
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-21 17:16:24 +0800

    feat: implement the type of History

--- client/src/types/paraphrase.ts
+++ client/src/types/paraphrase.ts
@@ -3,3 +3,11 @@ export type Paraphrase = {
     original_text: string,
     paraphrase_mode: string,
 }
+
+
+export type History = {
+    id: number,
+    original_text: string, 
+    transformed_text: string, 
+    created_at: string
+}

a3548fc2977e5bc42b89a3c295f403d251d5fc36
John Kristan Torremocha <johnkristan01@gmail.com>
2025-01-07 17:11:01 +0800

    feat: modified the theme state update

--- client/src/store/darkModeState.ts
+++ client/src/store/darkModeState.ts
@@ -3,10 +3,24 @@ import { create } from "zustand"
 
 interface DarkModeState{
     isDarkMode: boolean,
-    setIsDarkModeState: (isDarkMode: boolean) => void
+    updateThemeState: () => void
 }
 
 export const useDarkModeStore = create<DarkModeState>((set) => ({
-    isDarkMode: false,
-    setIsDarkModeState: (isDarkMode: boolean) => set({isDarkMode})
+    isDarkMode: localStorage.getItem('theme') === 'dark',
+    updateThemeState: () => {
+        set((state) => {
+            const newState = !state.isDarkMode; 
+            const htmlElement = document.documentElement;
+
+            const theme = newState ? 'dark' : 'light';
+
+            htmlElement.classList.toggle('dark', newState);
+            localStorage.setItem('theme', theme);
+
+
+            localStorage.setItem('theme', newState ? 'dark' : 'light');
+            return { isDarkMode: newState };
+        });
+    },
 }))

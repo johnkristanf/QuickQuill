498067da8e8787ec95db66ea96e99c5e1217712c
John Kristan Torremocha <johnkristan01@gmail.com>
2025-01-09 05:07:25 +0800

    feat: redeploy client

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import tailwindcssAnimate from "tailwindcss-animate";
-
+ 
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
